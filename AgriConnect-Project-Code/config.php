6465c0f2d9f2d096213c965817ed886ae9521146
HEMANTHCHOWDARY55 <123453166+HEMANTHCHOWDARY55@users.noreply.github.com>
2024-11-12 00:18:00 +0530

    Add files via upload

---
+++ AgriConnect-Project-Code/config.php
@@ -0,0 +1,35 @@
+<?php
+$config = [
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'root',
+    'DB_PASS' => '',
+    'DB_NAME' => 'agriconnect',
+
+    'MAILER_SERVER' => 'ded723.hostwindsdns.com',
+    'MAILER_LOGINID' => 'agri@projectmailer.xyz',
+    'MAILER_PASSWORD' => 'S9JPDXdAtBdJ',
+    'MAILER_PORT' => '465',
+
+    'PROJECT_TITLE' => 'AgriConnect',
+    'PROJECT_TAGLINE' => 'Integrated Agriculture Web Platform',
+
+    'CURRENCY_SYMBOL' => '₹',
+
+    'ADDRESS' => ['Ongole','PIN 523001','Andhra Pradesh,India'],
+    'PHONE' => ['+91-9391038757'],
+    'EMAIL' => ['info@AgriConnect.com'],
+    'WEB_ADDRESS' => 'www.AgriConnect.com',
+    'LOCATION_MAP' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127482.6881405034!2d101.6169488902334!3d3.138503559663121!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc49c701efeae7%3A0xf4d98e5b2f1c287d!2sKuala%20Lumpur%2C%20Federal%20Territory%20of%20Kuala%20Lumpur%2C%20Malaysia!5e0!3m2!1sen!2sin!4v1589645210749!5m2!1sen!2sin',
+    'ENQUIRY_MAIL' => 'chidipothumounisha07@gmail.com',
+    'NO_REPLY_MAIL' => 'chidipothumounisha07@gmail.com',
+
+    'SOCIAL_TWITTER' => '#',
+    'SOCIAL_FACEBOOK' => '#',
+    'SOCIAL_INSTAGRAM' => '#',
+    'SOCIAL_SKYPE' => '#',
+    'SOCIAL_LINKEDIN' => '#',
+    'SOCIAL_YOUTUBE' => '#',
+    'DEVELOPERS' => ['Mounisha','Dwaraka Mai','Nagalakshmi Kumari','Kasim Saida'],
+    
+];
+?>
